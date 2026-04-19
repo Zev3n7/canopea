db99af95212704bcdbf446328012369616fe4256
Zev3n7 <logan.benitez15.0@gmail.com>
2026-04-19 15:25:11 -0600

    error con girdref

--- src/components/MagicBento.tsx
+++ src/components/MagicBento.tsx
@@ -494,7 +494,7 @@ const BentoCardGrid: React.FC<{
   <div
     className="bento-section grid gap-2 p-3 max-w-[54rem] select-none relative"
     style={{ fontSize: 'clamp(1rem, 0.9rem + 0.5vw, 1.5rem)' }}
-    ref={gridRef}
+    ref={gridRef as React.RefObject<HTMLDivElement>}
   >
     {children}
   </div>
